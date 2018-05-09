8e585fb1866dbe3621166d95a67a2e418c41c91b
paulVer777 <pawel.gitara@onet.eu>
2018-05-09 11:34:23 +0200

    przesył do bazy danych

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
 import React, {Component} from 'react';
-import './App.css';
 
 
 class App extends Component {
+
+
+    state = {
+
+        counter: 0
+
+    };
+
+    componentDidMount() {
+
+        fetch('https://jfddl4-sandbox.firebaseio.com/pawelp/counter/.json')
+            .then(response => response.json())
+            .then(acctualCounterValFromDb => this.setState({
+
+                counter: acctualCounterValFromDb
+
+            }))
+
+    }
+
+
+    saveToDb = () => fetch(
+        'https://jfddl4-sandbox.firebaseio.com/pawelp/counter/.json',
+        {
+            method: 'PUT', //metoda przesyłu danych do bazy
+            body: JSON.stringify(this.state.counter) //to co przesyłamy, dodatkowo konwersja na json
+
+        }
+    );
+
+
+    decHandler = () => {
+
+        this.setState({
+
+            counter: this.state.counter - 1
+
+
+        }, this.saveToDb) // "save to db" wykona sie kiedy stan sie zmieni, drugi argument wykona sie kiedy stan sie zmieni
+
+    };
+
+    incHandler = () => {
+
+        this.setState({
+
+            counter: this.state.counter + 1
+
+
+        }, this.saveToDb)
+
+    };
+
 
     render() {
 
@@ -11,10 +63,19 @@ class App extends Component {
 
             <div>
 
+                <h1>
+                    {this.state.counter}
+
+                </h1>
+
+                <button onClick={this.decHandler}>-</button>
+                <button onClick={this.incHandler}>+</button>
+
+
             </div>
 
 
-        );
+        )
     }
 }
 
