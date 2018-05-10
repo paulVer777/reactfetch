cc1bc7e3b36e0436a64a98fc6520d4e0055d23fa
paulVer777 <pawel.gitara@onet.eu>
2018-05-10 13:47:01 +0200

    pobranie i wyswietlenie wiadomosci na ekranie

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,40 +3,50 @@ import React from 'react'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import {database} from "../firebase"; // importujemy biblioteke firebase
-
+import {mapObjectToArray, utils} from '../utils'
+import MenuItem from 'material-ui/MenuItem';
 
 class Chat extends React.Component {
 
     state = {
-        name: 'Mateusz',
+        name: 'Gal Anonim',
         newMessage: '',
-        messages: null
+        messages: []
     };
-   sfi=(event,value)=>(
-       this.setState({
-       newMessage:value
-       })
-   );
 
 
-   addMessage=()=> database.ref('/chat').push({
+    sfi = (event, value) => (
+        this.setState({
+            newMessage: value
+        })
+    );
 
 
-       message:this.state.newMessage,
-       user:this.state.name,
-       timestamp:Data.now()
+    componentDidMount() {
+
+        database.ref('/chat') //wskazuje na miejsce w bazie danych, wystarczy ukosnik bo glowny adres zapisany jest w firebase js.
+            .on( //jesli stan sie zmieni, zrob cos raz(once) lub (cyklicznie)
+                'value',
+                (snapshot) => { //zawiera migawke aktualnego stanu z bazy danych, aktualny stan
+
+                    this.setState({
+
+                        messages: mapObjectToArray(snapshot.val()) //konwertuje na tablice z obiektami
+                    })
+                }
+            )
+    }
 
 
+    addMessage = () => database.ref('/chat').push({
 
 
-   })
+        message: this.state.newMessage,
+        user: this.state.name,
+        timestamp: Date.now()
 
 
-
-
-
-
-
+    })
 
 
     render() {
@@ -54,8 +64,24 @@ class Chat extends React.Component {
                     primary={true}
                     fullWidth={true}
                 />
+                {/*<h1>{this.state.messages}</h1>*/}
+
+
+                <div>
+
+                    {
+                        this.state.messages.map((value, key) => (
+                            <MenuItem
+
+                                primaryText={`${value.user}: ${value.message}`}
+
+                            />
+                        ))
+                    }
+                </div>
             </div>
         )
     }
 }
+
 export default Chat
