dcd6eeb7e4c7640390419701fecdcd273bc6126d
paulVer777 <pawel.gitara@onet.eu>
2018-05-11 14:46:32 +0200

    complete app

--- src/Components/Auth/LoginForms.js
+++ src/Components/Auth/LoginForms.js
@@ -6,9 +6,25 @@ import LoginByEmailAndPassword from "../LoginByEmailAndPassword";
 
 class LoginForms extends React.Component {
 
+    state={
+
+    logInEmail:'',
+    logInPassword:''
+    };
+
     LoginByGoogle = () => {
         auth.signInWithPopup(googleProvider)
     } //wyswietli okno logowania do google
+
+
+    loginByEmailAndPassword = () => auth.signInWithEmailAndPassword(
+        this.state.logInEmail,
+        this.state.logInPassword
+    ).catch(e=>alert('something went wrong!!!'))
+
+
+    onLogInEmailChange = (e, value) => this.setState({logInEmail: value})
+    onLogInPasswordChange = (e, value) => this.setState({logInPassword: value}) //login forms obsluguje caly stan
 
 
     render() {
@@ -16,20 +32,16 @@ class LoginForms extends React.Component {
 
         return (
 
-
             <div>
-
-
                 <LoginByEmailAndPassword
 
-                    emailValue={''}
-                    onEmailChange={() => {
-                    }}
-                    passwordValue={''}
-                    onPasswordChange={() => {
-                    }}
-                    onLogInClick={() => {
-                    }}
+                    emailValue={this.state.logInEmail}
+                    onEmailChange={this.onLogInEmailChange}
+                    passwordValue={this.state.logInPassword}
+                    onPasswordChange={this.onLogInPasswordChange
+                    }
+                    onLogInClick={this.loginByEmailAndPassword
+                    }
 
                 />
 
@@ -37,17 +49,15 @@ class LoginForms extends React.Component {
                     onLogInClick={this.LoginByGoogle}
                 />
 
-                <LoginByGoogle
 
-                    onLogInClick={this.LoginByGoogle}
-
-                />
 
             </div>
-
         )
     }
 }
 
-
 export default LoginForms
+
+
+
+
